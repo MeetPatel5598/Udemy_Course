898de21aa3d1b6fc2e76882be00d74b1de3b3f50
Meet Patel <patel00446@gmail.com>
2021-04-14 14:21:11 -0400

    npm init is used

--- app.js
+++ app.js
@@ -1,15 +1,9 @@
 const http = require('http');
 
-const server = http.createServer(function(req,res){
-    if(req.url === '/'){
-        res.write('<html>');
-        res.write('<head><title>Assignment 1</title></head>');
-        res.write(
-          '<body><form action="/create-user" method="POST"><input type="text" name="username"><button type="submit">Send</button></form></body>'
-        );
-        res.write('</html>');
-        return res.end();
-    }
-});
+const routes = require('./routes');
+
+console.log(routes.someText);
+
+const server = http.createServer(routes.handler);
 
 server.listen(3000);
